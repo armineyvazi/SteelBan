a6386f44ce2d0304b640db7633771211f898283d
armineyvazi <genius2001armin@gmail.com>
2022-11-20 19:05:52 +0330

    Create Route for Auth login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * @see AuthController::class
+ * @methodName register
+ */
+//public
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login',[AuthController::class,'logIn']);
+
+
 });
+//private
+Route::middleware(['auth:sanctum'])->prefix('v1')->group( function( ) {
+    Route::post('logout',[AuthController::class,'logout']);//Route For Logout.
+});
+//Route::group(['middleware'=>['auth:sanctum']],function (){
+//
+//});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    Route::post('/logout',[AuthController::class,'logout']);
+//    return $request->user();
+//});
+
